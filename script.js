d231517115e276e02231def8b4664d68ce5ba0ed
Izadora Braga <izadora_abraga@outlook.com>
2023-09-11 19:00:18 -0300

    requisito 3 e 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('botao');
+
+botao.addEventListener('click', () => {
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
